cb4803a513590af4a51ec1d0214ac953ad1f6562
giannicau <gianni.cau@libero.it>
2014-10-30 13:44:21 +0100

    aggiunta la funzone per salvare

---
+++ save.js
@@ -0,0 +1,25 @@
+var saveButton = document.getElementById('save');
+
+saveButton.addEventListener ('click', saveImage);
+
+function saveImage(){
+	
+	var data = canvas.toDataURL();
+	//aèrire l'immagine come popup
+	//window.open(data, '_blank','location=0','menubar=0');
+	
+	var request = new XMLHttpRequest();
+	request.open('POST','save.php',true);
+	request.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+	request.send('img='+data);
+	
+	request.onreadystatechange = function(){
+		if(request.readyState == 4 && request.status = 200){
+			var responde = request.responseText;
+			window.open(response, '_blank','location=0','menubar=0');
+	
+		}
+	}
+	
+	
+}
