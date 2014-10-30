dfe5e6a67ec8185cc8b2d01ff7aaf9953d986138
root <gianni.cau@libero.it>
2014-10-30 13:18:46 +0000

    ultima correzione a get

--- save.js
+++ save.js
@@ -9,18 +9,20 @@ function saveImage(){
 	//window.open(data, '_blank','location=0','menubar=0');
 	
 	var request = new XMLHttpRequest();
-	request.open('POST','save.php',true);
-	request.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-	request.send('img='+data);
+	
 	
 	request.onreadystatechange = function(){
 		if(request.readyState == 4 && request.status == 200){
 			var response = request.responseText;
 			console.log(response);
-			window.open('download.php?file='+response, '_blank','location=0','menubar=0');
+			//window.open('download.php?file='+response, '_blank','location=0','menubar=0');
 	
 		}
 	}
+	
+	request.open('POST','save.php',true);
+	request.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+	request.send('img ='+data);
 	
 	
 }
