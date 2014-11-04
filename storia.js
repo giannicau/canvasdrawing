196f673382054028ddf4f3d357ca8c92daf87760
giannicau <gianni.cau@libero.it>
2014-11-04 22:56:03 +0100

    modifica down immagine

--- storia.js
+++ storia.js
@@ -3,11 +3,7 @@ var annulla = document.getElementById('annulla'),
 	
 	var images {};
 	
-	var image = context.getImageData(0,0,canvas.width,canvas.height);
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	context.putImageData(image,0,0);
-	
+		
 	
 annulla.addEventListener('click', function(){
 	
