8737e9cb1459676d5ce57551923b9a12b4f752b7
giannicau <gianni.cau@libero.it>
2014-11-04 20:43:19 +0100

    modifica down immagine

--- save.js
+++ save.js
@@ -6,21 +6,20 @@ function saveImage(){
 	
 	var data = canvas.toDataURL();
 	//aprire l'immagine come popup
-	window.open(data, '_blank','location=0','menubar=0');
+	//window.open(data, '_blank','location=0','menubar=0');
 	
 	
 	var request = new XMLHttpRequest();
-	
 	
 	request.onreadystatechange = function(){
 		if(request.readyState == 4 && request.status == 200){
 			var response = request.responseText;
 			console.log(response);
-			window.open(response, '_blank','location=0','menubar=0');
+			window.open('download.php?file='+response, '_blank','location=0','menubar=0');
 	
 		}
 		
-		console.log('nn funge');
+		//console.log('nn funge');
 	}
 	
 	request.open('POST','save.php',true);
