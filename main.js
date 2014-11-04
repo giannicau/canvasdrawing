7ca70b85e4bd0f2d954b738f482c18967ac2805c
giannicau <gianni.cau@libero.it>
2014-11-04 17:08:32 +0100

    Update main.js
    aggiunta la parte touch

--- main.js
+++ main.js
@@ -39,3 +39,56 @@ var disengage = function(){
 canvas.addEventListener('mousedown', engage);
 canvas.addEventListener('mousemove', putPoint);
 canvas.addEventListener('mouseup', disengage);
+
+
+//parte touch
+
+// create a drawer which tracks touch movements
+		var drawer = {
+			isDrawing: false,
+			touchstart: function(coors){
+				context.beginPath();
+				context.moveTo(coors.x, coors.y);
+				this.isDrawing = true;
+			},
+			touchmove: function(coors){
+				if (this.isDrawing) {
+			        context.lineTo(coors.x, coors.y);
+			        context.stroke();
+				}
+			},
+			
+			touchend: function(coors){
+				if (this.isDrawing) {
+			        this.touchmove(coors);
+			        this.isDrawing = false;
+				}
+			}
+		}; //fine drower
+		// create a function to pass touch events and coordinates to drawer
+		function draw(event){
+			// get the touch coordinates
+			var coors = {
+				x: event.targetTouches[0].pageX,
+				y: event.targetTouches[0].pageY
+			};
+			// pass the coordinates to the appropriate handler
+			drawer[event.type](coors);
+		}
+		
+			
+		
+		// attach the touchstart, touchmove, touchend event listeners.
+	    canvas.addEventListener('touchstart',draw, false);
+	    canvas.addEventListener('touchmove',draw, false);
+	    canvas.addEventListener('touchend',draw, false);
+		
+				
+		
+		
+		
+		
+		// prevent elastic scrolling
+		document.body.addEventListener('touchmove',function(event){
+			event.preventDefault();
+		},false);	// end body.onTouchMove
